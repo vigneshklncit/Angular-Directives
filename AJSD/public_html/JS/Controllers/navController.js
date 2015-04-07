7aad323b60673dd8ef5ab6c0cb0f58ddcda88f52
Shekar <chandu537@gmail.com>
2015-04-08 00:56:56 +0530

    Controller initialized.

---
+++ AJSD/public_html/JS/Controllers/navController.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var horizontalNav  = angular.module("horizontalNavigation",[]);
+horizontalNav.controller("hnavController",['$scope',function($scope){
+       $scope.ctrlName = "Horizontal Navigation";
+}]);
+
